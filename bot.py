cd9bd51c047145f095d8c8788cb0cbe55d528ebe
jatdipesh03 <jatdipesh03@gmail.com>
2026-03-09 21:41:49 +0530

    clean bot code

--- bot.py
+++ bot.py
@@ -1,13 +1,6 @@
 import requests
 import time
 
-balance = 10000
-position = None
-entry_price = 0
-
-take_profit = 40
-stop_loss = 20
-
 prices = []
 
 url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
@@ -24,7 +17,6 @@ def ema(data, period):
 
 
 while True:
-
     r = requests.get(url)
     data = r.json()
 
@@ -42,23 +34,11 @@ while True:
         prices.pop(0)
 
     if len(prices) > 21:
-
         ema9 = ema(prices[-9:], 9)
         ema21 = ema(prices[-21:], 21)
 
         print("Price:", price)
         print("EMA9:", ema9)
         print("EMA21:", ema21)
-
-        if ema9 > ema21 and position is None:
-            position = "BUY"
-            entry_price = price
-            print("BUY at", price)
-
-        elif ema9 < ema21 and position == "BUY":
-            profit = price - entry_price
-            balance += profit
-            position = None
-            print("SELL at", price, "Profit:", profit)
 
     time.sleep(5)
