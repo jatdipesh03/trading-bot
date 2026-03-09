414247c61c3aea7f1fc821208932e3fab666dd3d
jatdipesh03 <jatdipesh03@gmail.com>
2026-03-10 00:43:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,45 +8,28 @@ app = Flask(__name__)
 
 print("SERVER STARTED")
 
-api_key = os.getenv("DELTA_API_KEY")
-api_secret = os.getenv("DELTA_API_SECRET")
-
-exchange = ccxt.delta({
-    "apiKey": api_key,
-    "secret": api_secret,
-    "enableRateLimit": True
-})
+exchange = ccxt.delta()
 
 symbol = "BTC/USDT"
 
-def trading_bot():
-
+def bot_loop():
     print("BOT LOOP STARTED")
-
     while True:
-
         try:
-
             ticker = exchange.fetch_ticker(symbol)
-            price = ticker["last"]
-
-            print("BTC PRICE:", price)
-
+            print("BTC PRICE:", ticker["last"])
         except Exception as e:
-
             print("ERROR:", e)
 
         time.sleep(10)
-
 
 @app.route("/")
 def home():
     return "Bot Running"
 
-
 if __name__ == "__main__":
-
-    thread = threading.Thread(target=trading_bot)
-    thread.start()
+    t = threading.Thread(target=bot_loop)
+    t.daemon = True
+    t.start()
 
     app.run(host="0.0.0.0", port=10000)
